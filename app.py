38da1817e3c64ae1172fca94bac64d3ed6ee8160
tartrsn <tartrsn@gmail.com>
2021-05-31 00:03:36 +0300

    changed state for setup

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from vocabulizer import db, create_app
 from vocabulizer.settings import PORT
 from vocabulizer.jobs import jobs_scheduler
 
+jobs_scheduler.run()
 app = create_app()
 
 if __name__ == "__main__":
-    # db.create_all(app=create_app())
-    jobs_scheduler.run()
-    app.run(port=PORT)
+    db.create_all(app=create_app())
+    # app.run(port=PORT)
 
